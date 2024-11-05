47fd86bbac2b3292628fc3b8e6c6ec437684ed9d
= <=>
2024-11-05 09:37:36 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,165 @@
+import { Authenticated, GitHubBanner, Refine } from "@refinedev/core";
+import { DevtoolsPanel, DevtoolsProvider } from "@refinedev/devtools";
+import { RefineKbar, RefineKbarProvider } from "@refinedev/kbar";
+
+import {
+  ErrorComponent,
+  ThemedLayoutV2,
+  ThemedSiderV2,
+  useNotificationProvider,
+} from "@refinedev/antd";
+import "@refinedev/antd/dist/reset.css";
+
+import { authProvider, dataProvider, liveProvider } from "./providers";
+import { Home, ForgotPassword, Login, Register, CompanyList } from "./pages";
+
+import routerBindings, {
+  CatchAllNavigate,
+  DocumentTitleHandler,
+  NavigateToResource,
+  UnsavedChangesNotifier,
+} from "@refinedev/react-router-v6";
+import { App as AntdApp } from "antd";
+
+import { BrowserRouter, Outlet, Route, Routes } from "react-router-dom";
+
+// import {
+//   BlogPostCreate,
+//   BlogPostEdit,
+//   BlogPostList,
+//   BlogPostShow,
+// } from "./pages/blog-posts";
+// import {
+//   CategoryCreate,
+//   CategoryEdit,
+//   CategoryList,
+//   CategoryShow,
+// } from "./pages/categories";
+import Layout from "./components/layout";
+import { resources } from "./config/resources";
+import Create from "./pages/company/create";
+import Edit from "./pages/company/edit";
+import List from "./pages/tasks/list";
+import CreateTask from "./pages/tasks/create";
+import EditTask from "./pages/tasks/edit";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <GitHubBanner />
+      <RefineKbarProvider>
+        <AntdApp>
+          <DevtoolsProvider>
+            <Refine
+              dataProvider={dataProvider}
+              liveProvider={liveProvider}
+              notificationProvider={useNotificationProvider}
+              routerProvider={routerBindings}
+              authProvider={authProvider}
+              resources={resources}
+              
+           
+              options={{
+                syncWithLocation: true,
+                warnWhenUnsavedChanges: true,
+                useNewQueryKeys: true,
+                projectId: "Wvn3Sc-jR6Cn5-m8eGne",
+                liveMode: "auto",
+              }}
+            >
+              <Routes>
+                {/* <Route
+                  element={
+                    <Authenticated
+                      key="authenticated-inner"
+                      fallback={<CatchAllNavigate to="/login" />}
+                    >
+                      <ThemedLayoutV2
+                        Sider={(props) => <ThemedSiderV2 {...props} fixed />}
+                      >
+                        <Outlet />
+                      </ThemedLayoutV2>
+                    </Authenticated>
+                  }
+                > */}
+                  {/* <Route
+                    index
+                    element={<NavigateToResource resource="blog_posts" />}
+                  /> */}
+                  {/* <Route path="/blog-posts">
+                    <Route index element={<BlogPostList />} />
+                    <Route path="create" element={<BlogPostCreate />} />
+                    <Route path="edit/:id" element={<BlogPostEdit />} />
+                    <Route path="show/:id" element={<BlogPostShow />} />
+                  </Route> */}
+                  {/* <Route path="/categories">
+                    <Route index element={<CategoryList />} />
+                    <Route path="create" element={<CategoryCreate />} />
+                    <Route path="edit/:id" element={<CategoryEdit />} />
+                    <Route path="show/:id" element={<CategoryShow />} />
+                  </Route>
+                  <Route path="*" element={<ErrorComponent />} />
+                </Route> */}
+                
+                {/* <Route
+                  element={
+                    <Authenticated
+                      key="authenticated-outer"
+                      fallback={<Outlet />}
+                    >
+                      <NavigateToResource />
+                    </Authenticated>
+                  }
+                >
+                 */}
+                  <Route path="/register" element={<Register />} />
+                  <Route path="/login" element={<Login />} />
+                  <Route path="/forgot-password" element={<ForgotPassword />} />
+                
+
+                {/* Route for layout */}
+                <Route
+                  element={
+                    <Authenticated
+                      key="authenticated-layout"
+                      fallback={<CatchAllNavigate to="/login" />}
+                    >
+                      <Layout>
+                        <Outlet />
+                      </Layout>
+                    </Authenticated>
+                  }>
+                  <Route index element={<Home />} />
+                   <Route path="/companies" >
+                    <Route index element={<CompanyList />} />
+                    <Route path="new" element={<Create />} />
+                    <Route path="edit/:id" element={<Edit />} />
+                   </Route>
+                   <Route path="/tasks" element={
+                     <List >
+                        <Outlet />
+                      </List>
+                      }>
+                        <Route path="new" element={<CreateTask />} />
+                        <Route path="edit/:id" element={<EditTask />} />
+                     
+                  
+                   </Route>
+
+
+                </Route>
+              </Routes>
+
+              <RefineKbar />
+              <UnsavedChangesNotifier />
+              <DocumentTitleHandler />
+            </Refine>
+            <DevtoolsPanel />
+          </DevtoolsProvider>
+        </AntdApp>
+      </RefineKbarProvider>
+    </BrowserRouter>
+  );
+}
+
+export default App;
